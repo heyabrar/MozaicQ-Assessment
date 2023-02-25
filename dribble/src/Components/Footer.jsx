b197a3d47e5f0711db7e5cec84432861a88c2b19
Abrar Khan <heyabrarkhan@gmail.com>
2023-02-25 17:17:36 +0530

    Pointer for footer added

--- dribble/src/Components/Footer.jsx
+++ dribble/src/Components/Footer.jsx
@@ -10,74 +10,74 @@ export default function Footer() {
                     <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Dribbble_Text_Logo_Script.svg/2560px-Dribbble_Text_Logo_Script.svg.png" alt="logo" width={{ base: '100px' }} />
                     <Text mt={{base : '20px'}}>Dribbble is the world’s leading community for creatives to share, grow, and get hired.</Text>
                     <Flex mt={{base : '20px'}}fontSize={{base : '22px'}} gap={{base : '20px'}}>
-                        <Text><CiBasketball /></Text>
-                        <Text><BsTwitter /></Text>
-                        <Text><BsFacebook /></Text>
-                        <Text><BsInstagram /></Text>
-                        <Text><BsPinterest /></Text>
+                        <Text cursor='pointer'><CiBasketball /></Text>
+                        <Text cursor='pointer'><BsTwitter /></Text>
+                        <Text cursor='pointer'><BsFacebook /></Text>
+                        <Text cursor='pointer'><BsInstagram /></Text>
+                        <Text cursor='pointer'><BsPinterest /></Text>
                     </Flex>
                 </Box>
 
                 <SimpleGrid columns={[2,3,3,5]} width={{base: '', lg : '80%'}} >
                     <Box lineHeight={{base :'35px'}}>
-                        <Text fontWeight='600'>For designers</Text>
-                        <Text>Go Pro!</Text>
-                        <Text>Explore design work</Text>
-                        <Text>Design blog</Text>
-                        <Text>Overtime podcasts</Text>
-                        <Text>Playoffs</Text>
-                        <Text>Weekly Warm-Up</Text>
-                        <Text>Refer a Friend</Text>
-                        <Text>Code of conduct</Text>
+                        <Text fontWeight='600' cursor='pointer'>For designers</Text>
+                        <Text cursor='pointer'>Go Pro!</Text>
+                        <Text cursor='pointer'>Explore design work</Text>
+                        <Text cursor='pointer'>Design blog</Text>
+                        <Text cursor='pointer'>Overtime podcasts</Text>
+                        <Text cursor='pointer'>Playoffs</Text>
+                        <Text cursor='pointer'>Weekly Warm-Up</Text>
+                        <Text cursor='pointer'>Refer a Friend</Text>
+                        <Text cursor='pointer'>Code of conduct</Text>
                     </Box>
 
                     <Box lineHeight={{base :'35px'}}>
                         <Text fontWeight={600}>Hire designers</Text>
-                        <Text>Post a job opening</Text>
-                        <Text>Post a freelance project</Text>
-                        <Text>Search for designers</Text>
-                        <Text fontWeight={600}>Brands</Text>
-                        <Text>Advertise with us</Text>
+                        <Text cursor='pointer'>Post a job opening</Text>
+                        <Text cursor='pointer'>Post a freelance project</Text>
+                        <Text cursor='pointer'>Search for designers</Text>
+                        <Text fontWeight={600} cursor='pointer'>Brands</Text>
+                        <Text cursor='pointer'>Advertise with us</Text>
                     </Box>
 
 
                     <Box lineHeight={{base :'35px'}}>
-                        <Text fontWeight={600}>Company</Text>
-                        <Text>About!</Text>
-                        <Text>Careers</Text>
-                        <Text>DSupport</Text>
-                        <Text>Media kit</Text>
-                        <Text>Testimonials</Text>
-                        <Text>API</Text>
-                        <Text>Terms of service</Text>
-                        <Text>Privacy policy</Text>
-                        <Text>Cookie policy</Text>
+                        <Text fontWeight={600} cursor='pointer'>Company</Text>
+                        <Text cursor='pointer'>About!</Text>
+                        <Text cursor='pointer'>Careers</Text>
+                        <Text cursor='pointer'>DSupport</Text>
+                        <Text cursor='pointer'>Media kit</Text>
+                        <Text cursor='pointer'>Testimonials</Text>
+                        <Text cursor='pointer'>API</Text>
+                        <Text cursor='pointer'>Terms of service</Text>
+                        <Text cursor='pointer'>Privacy policy</Text>
+                        <Text cursor='pointer'>Cookie policy</Text>
                     </Box>
 
 
                     <Box lineHeight={{base :'35px'}}>
                         <Text fontWeight={600}>Directories</Text>
-                        <Text>Design jobs!</Text>
-                        <Text>Designers for hire</Text>
-                        <Text>Freelance designers for hire</Text>
-                        <Text>Tags</Text>
-                        <Text>Places</Text>
-                        <Text fontWeight={600}>Design assets</Text>
-                        <Text>Dribbble Marketplace</Text>
-                        <Text>Creative Market</Text>
-                        <Text>Fontspring</Text>
-                        <Text>Font Squirrel</Text>
+                        <Text cursor='pointer'>Design jobs!</Text>
+                        <Text cursor='pointer'>Designers for hire</Text>
+                        <Text cursor='pointer'>Freelance designers for hire</Text>
+                        <Text cursor='pointer'>Tags</Text>
+                        <Text cursor='pointer'>Places</Text>
+                        <Text fontWeight={600} cursor='pointer'>Design assets</Text>
+                        <Text cursor='pointer'>Dribbble Marketplace</Text>
+                        <Text cursor='pointer'>Creative Market</Text>
+                        <Text cursor='pointer'>Fontspring</Text>
+                        <Text cursor='pointer'>Font Squirrel</Text>
                     </Box>
 
 
                     <Box lineHeight={{base :'35px'}}>
-                        <Text fontWeight={600}>Design Resources</Text>
-                        <Text>Freelancing</Text>
-                        <Text>Design Hiring</Text>
-                        <Text>Design Portfolio  </Text>
-                        <Text>Design Education</Text>
-                        <Text>Creative Process</Text>
-                        <Text>Design Industry Trends</Text>
+                        <Text fontWeight={600} cursor='pointer'>Design Resources</Text>
+                        <Text cursor='pointer'>Freelancing</Text>
+                        <Text cursor='pointer'>Design Hiring</Text>
+                        <Text cursor='pointer'>Design Portfolio  </Text>
+                        <Text cursor='pointer'>Design Education</Text>
+                        <Text cursor='pointer'>Creative Process</Text>
+                        <Text cursor='pointer'>Design Industry Trends</Text>
                     </Box>
                 </SimpleGrid>
             </Flex>
