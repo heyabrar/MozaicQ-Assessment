8ef1bea0437ba2fd360364bb06b617aab8855a8f
Abrar Khan <heyabrarkhan@gmail.com>
2023-02-25 15:39:38 +0530

    hireus video added

--- dribble/src/Pages/HomePage.jsx
+++ dribble/src/Pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import HireUsVideo from "../Components/HireUsVideo";
 import MainBodyImages from "../Components/MainBodyImages";
 import Navbar from "../Components/Navbar";
 import VideoComponent from "../Components/VideoComponent";
@@ -8,6 +9,7 @@ export default function HomePage ( ){
         <Navbar/>
         <VideoComponent/>
         <MainBodyImages/>
+        <HireUsVideo/>
         </>
     )
 }
