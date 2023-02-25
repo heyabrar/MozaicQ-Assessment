8f65132d27fe6d1db053b64bb8491ce26797bfec
Abrar Khan <heyabrarkhan@gmail.com>
2023-02-25 17:28:05 +0530

    Video controls on

--- dribble/src/Components/HireUsVideo.jsx
+++ dribble/src/Components/HireUsVideo.jsx
@@ -4,7 +4,7 @@ export default function HireUsVideo() {
     return (
         <>
             <Box width={{ base: '95%', lg: '45%' }} m='auto' mt={{ base: '30px' }}>
-                <video autoPlay loop muted style={{ borderRadius: '5px' }}>
+                <video autoPlay loop muted style={{ borderRadius: '5px' }} controls>
                     <source src="https://cdn.dribbble.com/userupload/4712235/file/original-d47d82220ea984760e99c5429294ae3a.mp4" />
                 </video>
             </Box>
